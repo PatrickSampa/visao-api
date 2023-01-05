1df36f177beb34f668bf8765f72c755abf2ebaae
moisesPompilio <moisesalexandrep.c@gmai.com>
2023-01-05 15:32:27 -0300

    comencando a construcao da classe login request

--- src/pytonRequest/loginSapiens/LoginRequestSapiens.ts
+++ src/pytonRequest/loginSapiens/LoginRequestSapiens.ts
@@ -1,11 +1,25 @@
+import axios from "axios";
 import { ILoginDTO } from "../../DTO/LoginDTO";
-import { parse } from 'node-html-parser';
+import { parse, HTMLElement  } from 'node-html-parser';
 
-export class RequestLoginSapiens{
-    constructor(private login: ILoginDTO) {}
-    async  handle(): Promise<string> {
+
+export class RequestLoginSapiens {
+    private urlSapiens = 'https://sapiens.agu.gov.br/';
+    private extesionUrlSapiens_loginCheck = 'login_check'
+    private extesionUrlSapiens_login = 'login'
+    private sessao = axios.create();
+    constructor(private login: ILoginDTO) { }
+    async handle(): Promise<string> {
         var cookie: string;
-
+        let token = await this.getInicialToken();
+        console.log("", token);
         return cookie
+    }
+    private async getInicialToken(): Promise<string> {
+        const getSapiensExternalPage = await this.sessao.get(`${this.urlSapiens}${this.extesionUrlSapiens_login}`);
+        const htmlPageLogin = getSapiensExternalPage.data;
+        const root: HTMLElement  = parse(htmlPageLogin);
+        const token = root.querySelector('input')!.getAttribute('value')!;
+        return token;
     }
 }
--- src/routes/InsertSapiensForSamir.routes.ts
+++ src/routes/InsertSapiensForSamir.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { insertSapiensMinutasController } from "../modules/InsertSapiensMinutas";
+import { loginController } from "../modules/LoginUsuario";
 
 //const sessao = request.session();
 
@@ -37,3 +38,33 @@ routerInsertSapiens.post("/insertMinutas", async (req, res) => {
 })
 
 
+/**
+ * @swagger
+ * /samir/login:
+ *   post:
+ *     summary: Inser minutas in the sapiens
+ *     tags: [InserMinuta]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Login'
+ *     responses:
+ *       200:
+ *         description: The user was successfully login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Coockie'
+ *                 
+ *       500:
+ *         description: Some server error
+ *       400:
+ *         description: The request error
+ */
+
+
+routerInsertSapiens.post("/login", async (req, res) => {
+    return loginController.handle(req, res);
+})

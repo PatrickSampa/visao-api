223cc85ab5691a7a04c9a4c78ce8dfd2c3820669
moisesPompilio <moisesjtmed@gmail.com>
2023-02-23 16:53:37 -0300

    correção de bug de insert de calculo

--- src/modules/UploadDocument/UploadDocumentUseCase.ts
+++ src/modules/UploadDocument/UploadDocumentUseCase.ts
@@ -1,4 +1,3 @@
-import { UploadDocumenteForSapienInPython } from "../../pytonRequest/UploadDocumenteForSapienInPython";
 import { RequestHeaderUploadArquivo } from "../../sapiensOperations/resquest/RequestHeaderUploadArquivo";
 import axios from 'axios';
 
@@ -6,12 +5,10 @@ import axios from 'axios';
 export class UploadDocumentUseCase {
 
     async execute(Coockie: string, fileName: string, conteudo: string, documento_id: string, tipo_documento: string): Promise<any> {
-        console.log("AQUI")
         const requestHeaderUploadArquivo = new RequestHeaderUploadArquivo;
         const headers = await requestHeaderUploadArquivo.execute({file_name: fileName, cookie: Coockie, tamanho: conteudo.length});
         const baseURL = `https://sapiens.agu.gov.br/upload_arquivo?documento=${documento_id}`
         const data = conteudo;
-        console.log(headers);
         return await axios.post(baseURL, data, {headers}).then(response =>{
             console.log(response.status)
             console.log(response.data)
